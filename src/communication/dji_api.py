67d71a064f47451a8e110105a80ce12f2a980b9a
ondralol <ondra.duba17@gmail.com>
2026-04-21 12:32:25 +0200

    Change drone api to async

--- src/communication/dji_api.py
+++ src/communication/dji_api.py
@@ -1,7 +1,7 @@
 import math
 from djitellopy import tello
 from enum import Enum, IntEnum, StrEnum
-
+import threading
 
 class DIRECTION(StrEnum):
     UP = "up"
@@ -24,77 +24,91 @@ class Drone:
     def __init__(self):
         self.drone = tello.Tello()
 
+    def _run(self, fn, *args):
+        """Runs function in another thread"""
+
+        # Only one thread can be running at the same time
+        if hasattr(self, '_worker') and self._worker.is_alive():
+            return  # ignore if command already running
+
+        self._worker = threading.Thread(target=fn, args=args, daemon=True)
+        self._worker.start()
+
     def connect(self):
         # TODO Log action
 
-        self.drone.connect()
+        self._run(self.drone.connect)
 
     def reboot(self):
         # TODO Log action
 
-        self.drone.reboot()
+        self._run(self.drone.reboot)
 
     def takeoff(self):
         # TODO Log action
 
-        self.drone.takeoff()
+        self._run(self.drone.takeoff)
     
     def land(self):
         # TODO Log action
 
-        self.drone.land()
+        self._run(self.drone.land)
     
     def startStream(self):
         # TODO Log action
 
-        self.drone.streamon()
+        self._run(self.drone.streamon)
 
     def stopStream(self):
         # TODO Log action
 
-        self.drone.streamoff()
+        self._run(self.drone.streamoff)
 
     def emergency(self):
         # TODO Log action
 
-        self.drone.emergency()
+        self._run(self.drone.emergency)
 
     def move(self, dir: DIRECTION, x: int):
         """Fly x cm in direction dir."""
         # TODO Log action
 
-        self.drone.move(dir, x)
+        self._run(lambda: self.drone.move(dir, x))
 
     def moveToXYZRelativeToCurrentPosition(self, x: int, y: int, z: int, speed: SPEED):
         #TODO Log action
 
-        self.drone.go_xyz_speed(x, y, z, speed)
+        self._run(lambda: self.drone.go_xyz_speed(x, y, z, speed))
 
     def moveToXYZRelativeToPad(self, x: int, y: int, z: int, speed: SPEED, pad: int):
         # TODO Log action
 
-        self.drone.go_xyz_speed_mid(x, y, z, speed, pad)
+        self._run(lambda: self.drone.go_xyz_speed_mid(x, y, z, speed, pad))
 
     def rotate(self, dir: ROTATION_DIRECTION, angle_deg: int):
         # TODO Log action
 
         if dir is ROTATION_DIRECTION.CLOCKWISE:
-            self.drone.rotate_clockwise(angle_deg)
+            self._run(lambda: self.drone.rotate_clockwise(angle_deg))
         elif dir is ROTATION_DIRECTION.COUNTERCLOCKWISE:
-            self.drone.rotate_counter_clockwise(angle_deg)
+            self._run(lambda: self.drone.rotate_counter_clockwise(angle_deg))
 
     def circleObject(self, radius_cm: int, speed: SPEED, steps: int = 24):
         """Circles around the object 
         
         Steps size dictates the circle smoothness
         """
-        for i in range(steps):
-            angle = 2 * math.pi * i / steps                                                                                                                                  
-            next_angle = 2 * math.pi * (i + 1) / steps                                                                                                                       
-            x = round(radius_cm * (math.cos(next_angle) - math.cos(angle)))
-            y = round(radius_cm * (math.sin(next_angle) - math.sin(angle)))                                                                                                    
-            self.moveToXYZRelativeToCurrentPosition(x, y, 0, speed)
-            self.rotate(ROTATION_DIRECTION.CLOCKWISE, 360 // steps)        
+
+        # Needs to be wrapped in order to be able to call in in a thread
+        def _circle():
+            for i in range(steps):
+                angle = 2 * math.pi * i / steps                                                                                                                                  
+                next_angle = 2 * math.pi * (i + 1) / steps                                                                                                                       
+                x = round(radius_cm * (math.cos(next_angle) - math.cos(angle)))
+                y = round(radius_cm * (math.sin(next_angle) - math.sin(angle)))                                                                                                    
+                self.drone.go_xyz_speed(x, y, 0, speed)
+                self.drone.rotate_clockwise(360 // steps)       
+        self._run(_circle)    
 
     def startSequence(self):
         # TODO
@@ -103,7 +117,7 @@ class Drone:
     def setSpeed(self, speed: SPEED):
         # TODO Log action
 
-        self.drone.set_speed(speed)
+        self._run(lambda: self.drone.set_speed(speed))
 
 
     def getBattery(self):
